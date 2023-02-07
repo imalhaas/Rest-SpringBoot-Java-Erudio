94d28e045ea369917e1d21e726163489b7a2ed13
imalhaas <lucasdecassia9@hotmail.com>
2023-02-07 19:58:15 -0300

    Start

---
+++ Rest-SpringBoot-Java-Erudio/src/main/java/com/API/RestSpringBootJavaErudio/RestSpringBootJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package com.API.RestSpringBootJavaErudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBootJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringBootJavaErudioApplication.class, args);
+	}
+
+}
---
+++ Rest-SpringBoot-Java-Erudio/src/test/java/com/API/RestSpringBootJavaErudio/RestSpringBootJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.RestSpringBootJavaErudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
