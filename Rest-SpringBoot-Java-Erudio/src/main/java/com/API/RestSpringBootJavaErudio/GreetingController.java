b43371a7f680444f16f2aaabb0ff2c68b4fcd839
imalhaas <lucasdecassia9@hotmail.com>
2023-02-07 21:29:19 -0300

    add

---
+++ Rest-SpringBoot-Java-Erudio/src/main/java/com/API/RestSpringBootJavaErudio/GreetingController.java
@@ -0,0 +1,22 @@
+package com.API.RestSpringBootJavaErudio;
+
+import jakarta.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private  final AtomicLong counter  = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "world") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
